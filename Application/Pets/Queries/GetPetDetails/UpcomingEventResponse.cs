611e5cf24e43ecbd45947d15c6337d438b3c55fe
Tomasz Paszek <tomasz.paszek98@gmail.com>
2025-07-25 17:56:32 +0200

    feat: Implement EntityNotFoundException and its handler for improved error handling

--- Application/Pets/Queries/GetPetDetails/UpcomingEventResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Application.Pets.Queries.GetPetDetails;
-
-public class UpcomingEventResponse
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public DateTime EventDate { get; set; }
-}
---
+++ PetFlow/ExceptionHandlers/NotFoundExceptionHandler.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Exceptions;
+using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+internal sealed class EntityNotFoundExceptionHandler(
+    IProblemDetailsService problemDetailsService,
+    ILogger<EntityNotFoundExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        if (exception is not EntityNotFoundException entityNotFoundException)
+        {
+            return false;
+        }
+
+        logger.LogError(exception, "Unhandled exception occurred");
+
+        httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+        
+        return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            Exception = exception,
+            ProblemDetails = new ProblemDetails
+            {
+                Title = "Entity not found.",
+                Detail = exception.Message
+            }
+        });
+    }
+}
